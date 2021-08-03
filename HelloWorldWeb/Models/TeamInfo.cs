64eec8f8fe18cceae61bd9f18c99638b282e832d
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-03 11:49:39 +0300

    TeamInfo changes

---
+++ HelloWorldWeb/Models/TeamInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HelloWorldWeb.Models
+{
+    public class TeamInfo
+    {
+        public string TeamName { get; set; }
+        public List<string> TeamMembers { get; set; }
+    }
+}
