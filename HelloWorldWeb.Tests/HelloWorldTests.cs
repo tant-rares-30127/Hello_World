afd49d4b62c74fb7ea3537cc060b1cb7e3a9fc65
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-11 10:31:04 +0300

    Fixed tests

--- HelloWorldWeb.Tests/HelloWorldTests.cs
+++ HelloWorldWeb.Tests/HelloWorldTests.cs
@@ -53,7 +53,7 @@ namespace HelloWorldWeb.Tests
 
             // Act
 
-            teamService.EditTeamMember(4, "NewName");
+            teamService.EditTeamMember(3, "NewName");
 
             // Assert
 
