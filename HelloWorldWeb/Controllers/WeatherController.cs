6284bde5dad97363809624291d58a532811d652c
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-12 10:53:05 +0300

    Added weathertype and records in the controller

--- HelloWorldWeb/Controllers/WeatherController.cs
+++ HelloWorldWeb/Controllers/WeatherController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloWorldWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,13 +11,17 @@ namespace HelloWorldWeb.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class WheatherController : ControllerBase
+    public class WeatherController : ControllerBase
     {
         // GET: api/<WheatherController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<DailyWeatherRecord> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new DailyWeatherRecord[]
+            {
+                new DailyWeatherRecord(new DateTime(2021, 8, 12), 22, WeatherType.Mild),
+                new DailyWeatherRecord(new DateTime(2021, 8, 13), 25, WeatherType.Hot),
+            };
         }
 
         // GET api/<WheatherController>/5
