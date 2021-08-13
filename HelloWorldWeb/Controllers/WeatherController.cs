b7e1cee97e6857ecc3474782891dbd58fd348da0
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-13 09:37:10 +0300

    Further cleaning

--- HelloWorldWeb/Controllers/WeatherController.cs
+++ HelloWorldWeb/Controllers/WeatherController.cs
@@ -34,12 +34,8 @@ namespace HelloWorldWeb.Controllers
         public IEnumerable<DailyWeatherRecord> ConvertResponseToWeatherRecordList(string content)
         {
             var json = JObject.Parse(content);
-            List<DailyWeatherRecord> result = new List<DailyWeatherRecord>();
-
             var jsonArray = json["daily"].Take(7);
-            result.AddRange(jsonArray.Select(CreatingWeatherRecordFromJToken));
-
-            return result;
+            return jsonArray.Select(CreatingWeatherRecordFromJToken);
         }
 
         private DailyWeatherRecord CreatingWeatherRecordFromJToken(JToken item)
