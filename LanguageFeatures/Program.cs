554b17908fccb3233d7fa211a2c230e964c10daa
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-19 11:07:13 +0300

    Experimenting with method as parameter

--- LanguageFeatures/Program.cs
+++ LanguageFeatures/Program.cs
@@ -22,7 +22,43 @@ namespace LanguageFeatures
 
             Member memberDeserealized = JsonSerializer.Deserialize<Member>(outputMember);
             Console.WriteLine(memberDeserealized.ToString());
-            
+
+            Console.Write("Insert your coffee:");
+            var type = Console.ReadLine();
+
+            Coffe coffe = MakeCoffe("grain", "milk", "water", "sugar", (type == "FlatWhite") ? (FlatWhite) : (Espresso));
+            Console.WriteLine($"Here is your coffee:{coffe}.");
+        }
+
+
+        static Coffe MakeCoffe(string grains, string milk, string water, string sugar, Func<string, string, string, string, Coffe> recipe)
+        {
+            try
+            {
+                Console.WriteLine("Start preparing coffee.");
+                var coffe = recipe(grains, milk, water, sugar);
+                return coffe;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finished.");
+            }
+        }
+
+        static Coffe Espresso(string grains, string milk, string water, string sugar)
+        {
+
+            return new Coffe("Espresso");
+        }
+
+        static Coffe FlatWhite(string grains, string milk, string water, string sugar)
+        {
+
+            return new Coffe("FlatWhite");
         }
     }
 }
