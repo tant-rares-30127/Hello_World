9815b4662387a5a1074cf08be788a81bf11e4623
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-19 09:46:44 +0300

    Language features

---
+++ LanguageFeatures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using HelloWorldWeb.Models;
+using HelloWorldWeb.Services;
+
+namespace LanguageFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITimeService timeService = new TimeService();
+            Member member = new Member("Member1", 5, timeService);
+            string jsonString = JsonSerializer.Serialize(member);
+            Console.WriteLine(jsonString);
+            string file = "MemberFile.json";
+            File.WriteAllText(file, jsonString);
+            Member memberDeserealized = JsonSerializer.Deserialize<Member>(jsonString);
+        }
+    }
+}
