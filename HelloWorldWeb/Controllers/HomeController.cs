f81074d4b59b927382eeed578a08778baf80614e
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-20 10:37:37 +0300

    Adding chat page, connection and signalr

--- HelloWorldWeb/Controllers/HomeController.cs
+++ HelloWorldWeb/Controllers/HomeController.cs
@@ -77,5 +77,10 @@ namespace HelloWorldWeb.Controllers
         {
             return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Chat()
+        {
+            return this.View();
+        }
     }
 }
