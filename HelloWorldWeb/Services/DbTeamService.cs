30889b874d00b9f0f526ab647e763addb6ce54c0
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-25 11:36:53 +0300

    Adding broadcast functionallity back

--- HelloWorldWeb/Services/DbTeamService.cs
+++ HelloWorldWeb/Services/DbTeamService.cs
@@ -10,22 +10,26 @@ namespace HelloWorldWeb.Services
     public class DbTeamService : ITeamService
     {
         private readonly ApplicationDbContext _context;
+        private readonly IBroadcastServices broadcastService;
 
-        public DbTeamService(ApplicationDbContext context)
+        public DbTeamService(ApplicationDbContext context, IBroadcastServices broadcastService)
         {
             _context = context;
+            this.broadcastService = broadcastService;
         }
 
         public void AddTeamMemberAsync(Member member)
         {
             _context.Add(member);
             _context.SaveChanges();
+            broadcastService.NewTeamMemberAdded(member.Name, member.Id);
         }
 
         public void DeleteTeamMember(Member member)
         {
             _context.Member.Remove(member);
             _context.SaveChanges();
+            this.broadcastService.DeleteTheTeamMember(member.Id);
         }
 
         public void EditTeamMember(int id, string name)
@@ -34,6 +38,7 @@ namespace HelloWorldWeb.Services
             member.Name = name;
             _context.Update(member);
             _context.SaveChanges();
+            this.broadcastService.EditTheTeamMember(id, name);
         }
 
         public TeamInfo GetTeamInfo()
